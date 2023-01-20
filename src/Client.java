7e7cd4b30c57380cbb1861af5b664786d29e2286
dima diachkov <diachkov.dima@gmail.com>
2023-01-20 18:30:32 +0200

    shutdown for client part

--- src/Client.java
+++ src/Client.java
@@ -8,6 +8,7 @@ public class Client implements Runnable{
     private Socket client;
     private BufferedReader in;
     private PrintWriter out;
+    private boolean done;
 
     @Override
     public void run() {
@@ -15,8 +16,27 @@ public class Client implements Runnable{
             client = new Socket("127.0.0.1", 9999);
             out = new PrintWriter(client.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            Inputhandler inHandler = new Inputhandler();
+            Thread t = new Thread(inHandler);
+            t.start();
+
+
         } catch (IOException e) {
 
+        }
+    }
+
+    public void shutdown() {
+        done = true;
+        try {
+            in.close();
+            out.close();
+            if (!client.isClosed()) {
+                client.close();
+            }
+        } catch (IOException e) {
+            //ignored
         }
     }
     class Inputhandler implements Runnable {
@@ -26,10 +46,16 @@ public class Client implements Runnable{
             try {
                 BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
                 while (!done) {
-
+                    String message = inReader.readLine();
+                    if (message.equals("/quit")) {
+                        inReader.close();
+                        shutdown();
+                    } else {
+                        out.println(message);
+                    }
                 }
             } catch (IOException e) {
-                //TODO
+                shutdown();
             }
         }
     }
